a92ce16de5910abc9f491efc67aeec1fd67e72be
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-02-02 02:59:12 +0600

    double-equal-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+//double equal fact
+const a = 2;
+const b = '2';
+if (a == b) { // it only check value
+    console.log("It's true");
+} else {
+    console.log("It's false");
+}
+
+//triple equal fact
+const a = 2;
+const b = '2';
+if (a === b) { // it check value and type
+    console.log("It's true");
+} else {
+    console.log("It's false");
+}
